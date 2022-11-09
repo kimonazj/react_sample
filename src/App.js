6e55151ee37e80909f3d6769157704613276d591
Ji (Anna) Zhang <65613966+kimonazj@users.noreply.github.com>
2022-11-09 18:54:45 -0500

    finish code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { useState } from "react";
+import React, { Component }  from 'react';
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem.js";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,18 +13,34 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [cart, setCart] = useState([])
+  const [total, setTotal] = useState(0)
+
+  //set the cart
+  function addToCart (item){
+    setCart([...cart, item.name]);
+    setTotal(total+item.price);
+  }
 
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        <BakeryItem curItem = {item} curIndex = {index} updateCart = {addToCart}/> // replace with BakeryItem component
       ))}
 
       <div>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+
+        {cart.map((item, index)=>(
+          <p>{item}</p>
+        ))}
+
+        <h2>Total</h2>
+        {total}
+        
+
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,26 @@
 // TODO: create a component that displays a single bakery item
+import { useState } from "react";
+import React from 'react'
+import "./BakeryItem.css";
+
+// curItem or props
+export default function BakeryItem({curItem, curIndex, updateCart}){
+    // or props.item.name
+    const curName = curItem.name;
+    const curDes = curItem.description;
+    const curPrice = curItem.price;
+    const curImage = curItem.image;
+    const [amount, setAmount] = useState(0)
+
+    return(
+        <div className="card">
+            <img src={curImage} className="cardimg"></img>
+            <div className="cardbody">
+                <h3 className="textPadding">{curName}</h3>
+                <p className="textPadding">{curDes}</p>
+                <p className="textPadding">${curPrice}</p>
+                <button onClick={()=>updateCart(curItem)}>Add to Cart</button>
+            </div>
+        </div>
+    )
+}
